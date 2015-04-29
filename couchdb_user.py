820108f71ef519275f2e93ab5be554079826c63c
SQiShER <SQiShER@users.noreply.github.com>
2015-04-30 00:08:05 +0200

    Refactored admin user creation

--- couchdb_user.py
+++ couchdb_user.py
@@ -11,17 +11,86 @@ try:
 except ImportError:
     HAS_REQUESTS = False
 
+
+class CouchDBError(Exception):
+    def __init__(self, status, response):
+        self.status_code = status
+        self.response = response
+
+
+class CouchDB:
+    def __init__(self, host="localhost", port="5984", auth_user=None, auth_password=None):
+        self.host = host
+        self.port = port
+        if auth_user is not None and auth_password is not None:
+            self.auth = (auth_user, auth_password)
+        else:
+            self.auth = None
+
+    def get_admin_user_config_url(self, username):
+        return "http://{0}:{1}/_config/admins/{2}".format(self.host, self.port, username)
+
+    def get_password_hash_of_admin_user(self, username):
+        r = requests.get(url=self.get_admin_user_config_url(username),
+                         headers={'Accept': 'application/json'},
+                         auth=self.auth)
+        if r.status_code == requests.codes.ok:
+            password_hash = r.json()
+            return password_hash
+        elif r.status_code == requests.codes.not_found:
+            return None
+        else:
+            response_body = r.json()
+            raise CouchDBError(r.status_code, response_body)
+
+    def create_or_update_admin_user(self, username, password, raw_password=False):
+        if raw_password:
+            params = {"raw": "true"}
+            current_password = self.get_password_hash_of_admin_user(username)
+            if current_password == password:
+                return False
+        else:
+            params = {}
+
+        r = requests.put(url=self.get_admin_user_config_url(username),
+                         data='"{0}"'.format(password),
+                         headers={'Accept': 'application/json'},
+                         params=params,
+                         auth=self.auth)
+
+        if r.status_code == requests.codes.ok:
+            return True
+        else:
+            raise CouchDBError(r.status_code, r.json())
+
+    def remove_admin_user(self, username):
+        r = requests.delete(url=self.get_admin_user_config_url(username),
+                            headers={'Accept': 'application/json'},
+                            auth=self.auth)
+
+        if r.status_code == requests.codes.ok:
+            return True
+        elif r.status_code == requests.codes.not_found:
+            return False
+        else:
+            response_body = r.json()
+            raise CouchDBError(r.status_code, response_body)
+
+
 def main():
     module = AnsibleModule(
-        argument_spec = dict(
-            host         = dict(default="localhost"),
-            port         = dict(default=5984),
-            name         = dict(required=True),
-            password     = dict(required=False),
-            raw_password = dict(type='bool', default='no'),
-            admin        = dict(type='bool', default='no'),
-            state        = dict(default="present", choices=["absent", "present"])
-        )
+        argument_spec=dict(
+            host=dict(type='str', default="localhost"),
+            port=dict(type='int', default=5984),
+            name=dict(type='str', required=True),
+            password=dict(type='str', required=False, no_log=True),
+            raw_password=dict(type='bool', choices=BOOLEANS, default='no'),
+            admin=dict(type='bool', default='no'),
+            state=dict(type='str', default="present", choices=["absent", "present"]),
+            auth_user=dict(type='str', required=False),
+            auth_password=dict(type='str', required=False, no_log=True)
+        ),
+        required_together=[['auth_user', 'auth_password']]
     )
 
     if not HAS_REQUESTS:
@@ -29,59 +98,33 @@ def main():
 
     host = module.params['host']
     port = module.params['port']
-    name = module.params['name']
+    username = module.params['name']
     password = module.params['password']
     raw_password = module.params['raw_password']
     admin = module.params['admin']
     state = module.params['state']
+    auth_user = module.params['auth_user']
+    auth_password = module.params['auth_password']
 
-    headers = {'Accept':'application/json'}
+    couchdb = CouchDB(host, port, auth_user, auth_password)
 
     if admin is True:
-        uri = 'http://{0}:{1}/_config/admins/{2}'.format(host, port, name)
-
         if state == "present":
-            
-            if raw_password == True:
-                r = requests.get(uri, headers=headers, auth=('couchdb','couchdb'))
-                current_password = r.json()
-                if current_password == password:
-                    module.exit_json(changed=False)
-
-            if raw_password:
-                params = {"raw":"true"}
-            else:
-                params = {}
-            data = '"{0}"'.format(password)
-            r = requests.put(uri, data=data, headers=headers, params=params, auth=('couchdb','couchdb'))
-            print r.url
-            
-            if r.status_code == requests.codes.ok:
-                module.exit_json(changed=True)
-            elif r.status_code == 401:
-                response_body = r.json()
-                reason = response_body['reason']
-                module.fail_json(msg=reason, response=response_body)
-            else:
-                response_body = r.json()
-                reason = "Unexpected status code: {0}".format(r.status_code)
-                module.fail_json(msg=reason, response=response_body)
+            try:
+                changed = couchdb.create_or_update_admin_user(username, password, raw_password)
+                module.exit_json(changed=changed)
+            except CouchDBError as e:
+                module.fail_json(msg=e.response["reason"], status_code=e.status_code, response=e.response)
         elif state == "absent":
-            
-            r = requests.delete(uri, headers=headers, auth=('couchdb','couchdb'))
+            try:
+                changed = couchdb.remove_admin_user(username)
+                module.exit_json(changed=changed)
+            except CouchDBError as e:
+                module.fail_json(msg=e.response["reason"], status_code=e.status_code, response=e.response)
+    else:
+        module.fail_json(msg="Support for regular users is work in progress...")
 
-            if r.status_code == requests.codes.ok:
-                module.exit_json(changed=True)
-            elif r.status_code == 401:
-                response_body = r.json()
-                reason = response_body['reason']
-                module.fail_json(msg=reason, response=response_body)                
-            elif r.status_code == 404:
-                module.exit_json(changed=False)
-            else:
-                response_body = r.json()
-                reason = "Unexpected status code: {0}".format(r.status_code)
-                module.fail_json(msg=reason, response=response_body)
 
 from ansible.module_utils.basic import *
+
 main()
