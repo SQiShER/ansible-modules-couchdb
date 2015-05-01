c2f1e20e9cbae715fee9a9f24f6d7d7d9d7ffbcd
SQiShER <SQiShER@users.noreply.github.com>
2015-05-01 19:53:48 +0200

    Changed authentication behavior to play well with Ansibles idempotent nature

--- couchdb_user.py
+++ couchdb_user.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python
+# -*- coding: utf-8 -*-
 
 try:
     import json
@@ -27,26 +28,32 @@ class CouchDB:
         else:
             self.auth = None
 
+    def get_auth(self, authenticated):
+        return self.auth if authenticated is True else None
+
     def get_admin_user_config_url(self, username):
         return "http://{0}:{1}/_config/admins/{2}".format(self.host, self.port, username)
 
-    def get_password_hash_of_admin_user(self, username):
+    def get_password_hash_of_admin_user(self, username, authenticated=False):
         r = requests.get(url=self.get_admin_user_config_url(username),
                          headers={'Accept': 'application/json'},
-                         auth=self.auth)
+                         auth=self.get_auth(authenticated))
         if r.status_code == requests.codes.ok:
             password_hash = r.json()
             return password_hash
+        elif r.status_code == requests.codes.unauthorized and not authenticated:
+            # Retry with authentication
+            return self.get_password_hash_of_admin_user(username, authenticated=True)
         elif r.status_code == requests.codes.not_found:
             return None
         else:
             response_body = r.json()
             raise CouchDBError(r.status_code, response_body)
 
-    def create_or_update_admin_user(self, username, password, raw_password=False):
+    def create_or_update_admin_user(self, username, password, raw_password=False, authenticated=False):
         if raw_password:
             params = {"raw": "true"}
-            current_password = self.get_password_hash_of_admin_user(username)
+            current_password = self.get_password_hash_of_admin_user(username, authenticated)
             if current_password == password:
                 return False
         else:
@@ -56,10 +63,13 @@ class CouchDB:
                          data='"{0}"'.format(password),
                          headers={'Accept': 'application/json'},
                          params=params,
-                         auth=self.auth)
+                         auth=self.get_auth(authenticated))
 
         if r.status_code == requests.codes.ok:
             return True
+        elif r.status_code == requests.codes.unauthorized and not authenticated:
+            # Retry with authentication
+            return self.create_or_update_admin_user(username, password, raw_password, authenticated=True)
         else:
             raise CouchDBError(r.status_code, r.json())
 
@@ -73,8 +83,7 @@ class CouchDB:
         elif r.status_code == requests.codes.not_found:
             return False
         else:
-            response_body = r.json()
-            raise CouchDBError(r.status_code, response_body)
+            raise CouchDBError(r.status_code, r.json())
 
 
 def main():
@@ -85,8 +94,9 @@ def main():
             name=dict(type='str', required=True),
             password=dict(type='str', required=False, no_log=True),
             raw_password=dict(type='bool', choices=BOOLEANS, default='no'),
-            admin=dict(type='bool', default='no'),
+            admin=dict(type='bool', choices=BOOLEANS, default='no'),
             state=dict(type='str', default="present", choices=["absent", "present"]),
+            force_basic_auth=dict(type='bool', choices=BOOLEANS, default='no'),
             auth_user=dict(type='str', required=False),
             auth_password=dict(type='str', required=False, no_log=True)
         ),
@@ -103,6 +113,7 @@ def main():
     raw_password = module.params['raw_password']
     admin = module.params['admin']
     state = module.params['state']
+    force_basic_auth = module.params['force_basic_auth']
     auth_user = module.params['auth_user']
     auth_password = module.params['auth_password']
 
@@ -111,7 +122,8 @@ def main():
     if admin is True:
         if state == "present":
             try:
-                changed = couchdb.create_or_update_admin_user(username, password, raw_password)
+                changed = couchdb.create_or_update_admin_user(username, password, raw_password,
+                                                              authenticated=force_basic_auth)
                 module.exit_json(changed=changed)
             except CouchDBError as e:
                 module.fail_json(msg=e.response["reason"], status_code=e.status_code, response=e.response)
